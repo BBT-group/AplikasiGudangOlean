2775411ed0e801c8b8b77f84ebb7556360ad8b48
ASUS\asus1 <covegaja5@gmail.com>
2024-07-26 14:34:24 +0700

    no message

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'databasegudangolean',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,

569390e93f96bf3d2cd1897cdccd8fcd5d58f0df
ASUS\asus1 <covegaja5@gmail.com>
2024-08-12 14:45:23 +0700

    no message

--- app/Views/v_kategori.php
+++ app/Views/v_kategori.php
@@ -31,7 +31,7 @@
                                                 <td><?= $sat['nama_kategori'] ?></td>
                                                 <td style="text-align: center;">
                                                     <a href="<?= base_url('satuan/deletesatuan/' . $sat['id_kategori']) ?>" class="btn btn-sm btn-danger" onclick="return  confirm(' Menghapus data akan menghapus barang yang bersangkutan Apakah anda yakin menghapus? ')"><i class="fas fa-trash"></i></a>
-                                                </td
+                                                </td>
                                                     </tr>
                                             <?php endforeach; ?>
                                     </tbody>
